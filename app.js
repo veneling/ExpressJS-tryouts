4b8c93505c4d313ab469cee873dbe2affba58866
Venelin Grozev <Venelin Grozev>
2016-10-18 21:49:58 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+let express = require('express')
+let path = require('path')
+let favicon = require('serve-favicon')
+let logger = require('morgan')
+let cookieParser = require('cookie-parser')
+let bodyParser = require('body-parser')
+let mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose
+  .connect('mongodb://localhost/test')
+  .then(() => {
+    console.log('connected to test database')
+  })
+
+let addImage = require('./routes/add-image')
+let listPictures = require('./routes/list-pictures')
+let searchByTag = require('./routes/search-by-tag')
+let searchInPeriod = require('./routes/search-in-period')
+
+let app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.locals.dateFormat = require('./date-format')
+app.use('/list', listPictures)
+app.use('/', addImage)
+app.use('/search-by-tag', searchByTag)
+app.use('/search-in-period', searchInPeriod)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.render('error', {
+      message: err.message,
+      error: err
+    })
+  })
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500)
+  res.render('error', {
+    message: err.message,
+    error: {}
+  })
+})
+
+
+module.exports = app
